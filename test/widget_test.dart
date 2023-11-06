0ba0ec153ee3edd99c7449977ff5f8307197ef5c
latiefdole <latiefdole@gmail.com>
2023-11-06 16:25:46 +0700

    test aja

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:kelas_4b/main.dart';
+import 'package:kelas_4b/widget/halaman_utama.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const HalamanUtama());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
