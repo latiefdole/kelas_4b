87543ca54cd04ec146f8f9b89943ded6f209107d
latiefdole <latiefdole@gmail.com>
2023-11-08 11:26:58 +0700

    Sidebar await navigator

--- lib/widget/Sidebar.dart
+++ lib/widget/Sidebar.dart
@@ -1,111 +1,109 @@
-  import 'package:flutter/material.dart';
-  import 'package:kelas_4b/ui/login.dart';
-  import 'package:kelas_4b/widget/halaman_utama.dart';
-  import 'package:kelas_4b/ui/pasien/pasien_page.dart';
-  import 'package:kelas_4b/ui/pegawai/pegawai_page.dart';
-  import 'package:kelas_4b/ui/poli/poli_page.dart';
+import 'package:flutter/material.dart';
+import 'package:kelas_4b/ui/login.dart';
+import 'package:kelas_4b/widget/halaman_utama.dart';
+import 'package:kelas_4b/ui/pasien/pasien_page.dart';
+import 'package:kelas_4b/ui/pegawai/pegawai_page.dart';
+import 'package:kelas_4b/ui/poli/poli_page.dart';
 
-  import '../helpers/user_info.dart';
+import '../helpers/user_info.dart';
 
-  class Sidebar extends StatelessWidget {
-    Sidebar({Key? key}) : super(key: key);
-    final UserInfo userInfo = UserInfo();
-    @override
-    Widget build(BuildContext context) {
-      return Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            // const DrawerHeader(
-            //   decoration: BoxDecoration(
-            //     color: Colors.blue,
-            //   ),
-            //   child:
-            // ),
-            UserAccountsDrawerHeader(
-              accountName: FutureBuilder<String>(
-                future: UserInfo().getNama(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Text('Loading...'); // Handle loading state
-                  } else if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}'); // Handle error
-                  } else {
-                    final namaUser = snapshot.data;
-                    return Text("$namaUser");
-                  }
-                },
-              ),
-              accountEmail: FutureBuilder<String>(
-                future: UserInfo().getUsername(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Text('Loading...'); // Handle loading state
-                  } else if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}'); // Handle error
-                  } else {
-                    final userEmail = snapshot.data;
-                    return Text("$userEmail");
-                  }
-                },
-              ),
+class Sidebar extends StatelessWidget {
+  Sidebar({Key? key}) : super(key: key);
+  final UserInfo userInfo = UserInfo();
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: [
+          // const DrawerHeader(
+          //   decoration: BoxDecoration(
+          //     color: Colors.blue,
+          //   ),
+          //   child:
+          // ),
+          UserAccountsDrawerHeader(
+            accountName: FutureBuilder<String>(
+              future: UserInfo().getNama(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Text('Loading...'); // Handle loading state
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}'); // Handle error
+                } else {
+                  final namaUser = snapshot.data;
+                  return Text("$namaUser");
+                }
+              },
             ),
+            accountEmail: FutureBuilder<String>(
+              future: UserInfo().getUsername(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Text('Loading...'); // Handle loading state
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}'); // Handle error
+                } else {
+                  final userEmail = snapshot.data;
+                  return Text("$userEmail");
+                }
+              },
+            ),
+          ),
 
-            ListTile(
-              leading: const Icon(Icons.home),
-              title: const Text('Home'),
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => const HalamanUtama()),
-                );
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.account_circle),
-              title: const Text('Pegawai'),
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const PegawaiPage(),
-                ));
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.local_hospital),
-              title: const Text('Pasien'),
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const PasienPage(),
-                ));
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.apartment),
-              title: const Text('Poli'),
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const PoliPage(),
-                ));
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.logout_rounded),
-              title: const Text("Keluar"),
-              onTap: () async {
-                await userInfo.logout(); // Call the logout function
-                // ignore: use_build_context_synchronously
-                Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(builder: (context) => const Login()),
-                  (Route<dynamic> route) => false,
-                );
-              },
-            )
-          ],
-        ),
-      );
-    }
-    
+          ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text('Home'),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => const HalamanUtama()),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.account_circle),
+            title: const Text('Pegawai'),
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const PegawaiPage(),
+              ));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.local_hospital),
+            title: const Text('Pasien'),
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const PasienPage(),
+              ));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.apartment),
+            title: const Text('Poli'),
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const PoliPage(),
+              ));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout_rounded),
+            title: const Text("Keluar"),
+            onTap: () async {
+              await userInfo.logout();
+              await Future.delayed(Duration.zero);
+              if (!context.mounted) return;
+              // Call the logout function
+              Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (context) => const Login()),
+                (Route<dynamic> route) => false,
+              );
+            },
+          )
+        ],
+      ),
+    );
   }
+}
